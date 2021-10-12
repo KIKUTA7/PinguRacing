5002825e1ce415336c514ca3d0def9962262df3a
Beka Kikutadze <kikutadze.beka@kiu.edu.ge>
2021-10-12 20:56:24 +0200

    Changes by Online Editor

--- src/pgdp/pinguracing/PinguRacing.java
+++ src/pgdp/pinguracing/PinguRacing.java
@@ -3,7 +3,49 @@ package pgdp.pinguracing;
 import static pgdp.MiniJava.*; //Diesen Import nicht löschen! Do not delete this import!
 
 public class PinguRacing {
+        public static int check(int x)
+    {
+        if(x>128) return 128;
+        else if(x<0) return 0;
+        else return x;
+    }
     public static void main(String[] args) {
+
+        int x= readInt("Alan starting position:");
+        int y=readInt("Bjarne starting position:");
+        int t=readInt("Race duration:");
+        int spx=0,spy=0;
+int i=0;
+while (i<(t/2))
+{
+    if (x%10==4) {spx=spx / 2 + 1;
+        spx = check(spx);
+    x+=spx;}
+    else {spx+=7;spx = check(spx);x+=spx;}
+    if(y%13==0) {spy=spy*2+1;spy = check(spy);y+=spy;
+    }
+    else {spy+=3;spy = check(spy);y+=spy;}
+    System.out.println("t = "+i);
+    System.out.println("Alan position = "+x+"; speed = "+spx);
+    write("Bjarne position = "+y+"; speed = "+spy);
+    i++;
+}
+while (i<t)
+{
+    if(i==(t*3)/5 || i==(t*4)/5) {spx=spx/4;spx=check(spx);x+=spx;}
+    else if(i==(t*4)/5) {spx=spx/4;spx=check(spx);x+=spx;}
+    else {spx++;spx=check(spx);x+=spx;}
+    if(i>=t-13) {spy=spy/2;spy=check(spy);y+=spy;}
+    else {spy--;spy=check(spy);y+=spy;}
+    System.out.println("t = "+i);
+    System.out.println("Alan position = "+x+"; speed = "+spx);
+    write("Bjarne position = "+y+"; speed = "+spy);
+    i++;
+
+}
+if(x>y) {write("Alan wins!");}
+else write("Bjarne wins!");
+
 
     }
 }
